80eac05db31ba2848f33625b78cb7eb0c6d619db
amitraut <amitram902@gmail.com>
2018-06-14 08:13:01 +0530

    python coding git assignment 01

---
+++ git_assignment.py
@@ -0,0 +1,25 @@
+git init
+
+git config-globaluser.name"your name"
+
+git config-globaluser."email"
+
+git init
+
+git add  assignment1.py
+
+git add .  /to add all file/
+
+git status
+
+git commit-m"your message"
+
+git  log 
+
+git status
+
+git remote add origin https://github.com/Amit902/assignment1.py.git
+
+git push -u origin master
+
+we are done !!
